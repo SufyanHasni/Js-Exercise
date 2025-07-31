c911608a5fdc6b670d14b834e81ff2ab1b41c66a
SufyanHasni <sufyanhasni2299@gmail.com>
2025-07-31 19:40:56 +0500

    Update chapter 5.js

---
+++ chapter 5.js
@@ -0,0 +1,47 @@
+                               // Chapter 2 / Math Expressions 
+
+        // Question 1 !
+        
+var num1 = 3;
+
+var num2 = 5;
+
+var sum = num1 + num2 ;
+
+document.write("The Sum Of" + num1 + "And" + num2 + "Is :" + sum );
+
+        // Question 2 !
+
+var num1 = 5;
+var num2 = 7;
+
+var addition = num1 + num2 ;
+var subtraction = num1 - num2 ;
+var multiplication = num1 * num2 ;
+var division = num1 / num2 ;
+var modulus = num1 % num2 ;
+
+document.write ("Addition :" + num1 + "+" + num2 + "=" + addition + "<br>");
+document.write ("Subtraction :" + num1 + "-" + num2 + "=" + subtraction + "<br>");
+document.write ("Multiplication :" + num1 + "*" + num2 + "=" + multiplication + "<br>");
+document.write ("Division :" + num1 + "/" + num2 + "=" + division + "<br>");
+document.write ("Modulus :" + num1 + "%" + num2 + "=" + modulus + "<br>");
+
+        // Question 3 !
+
+
+        // Question 4 !
+
+var ticketPrice = 600;
+
+var totalTickets = 5 ;
+
+var totalCost = ticketPrice * totalTickets;
+
+document.write ("Total Cost To Buy " + totalTickets + " " +"Movie ticket is" + totalCost + "PKR.");
+
+        // Question 5 !
+
+
+
+
